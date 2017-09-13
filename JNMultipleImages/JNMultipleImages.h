fd55ce37b1112d6a5d5403a6ac8241f5418638c7
mohammadnabulsi <mohammad.s.nabulsi@gmail.com>
2017-09-13 14:45:29 +0300

    Initial Commit

---
+++ JNMultipleImages/JNMultipleImages.h
@@ -0,0 +1,19 @@
+//
+//  JNMultipleImages.h
+//  JNMultipleImages
+//
+//  Created by Mohammad Nabulsi on 9/13/17.
+//  Copyright © 2017 Mohammad Nabulsi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JNMultipleImages.
+FOUNDATION_EXPORT double JNMultipleImagesVersionNumber;
+
+//! Project version string for JNMultipleImages.
+FOUNDATION_EXPORT const unsigned char JNMultipleImagesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JNMultipleImages/PublicHeader.h>
+
+

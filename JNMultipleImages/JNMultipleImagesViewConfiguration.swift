63175e2a7a588bdd2ff833c4a6ecea7f08ca3e60
RolaSal99 <rola.salahat@harri.com>
2024-07-22 18:29:05 +0300

    Add JNMultipleImagesViewConfiguration     - Code review

---
+++ JNMultipleImages/JNMultipleImagesViewConfiguration.swift
@@ -0,0 +1,9 @@
+//
+//  JNMultipleImagesViewConfiguration.swift
+//  JNMultipleImages
+//
+//  Created by Rola Salahat on 02/07/2024.
+//  Copyright © 2024 Mohammad Nabulsi. All rights reserved.
+//
+
+import Foundation
